230ee067dce15d13dc54edfcaf19ec6683543d0f
timmson <timmson666@mail.ru>
2022-02-02 23:51:11 +0300

    Initial commit

--- src/rover-reducer.js
+++ src/rover-reducer.js
@@ -1,83 +1,44 @@
-import {FORWARD, BACKWARD, TURN_RIGHT, TURN_LEFT} from "./actions"
+import {BACKWARD, FORWARD, TURN_LEFT, TURN_RIGHT} from "./actions"
 import {NORTH, WEST} from "./directions"
 import {SPEED} from "./settings"
+
+function turn(state, clockwise) {
+	let newDirection = state.direction + clockwise
+	if (newDirection < NORTH) newDirection = WEST
+	if (newDirection > WEST) newDirection = NORTH
+	return {
+		x: state.x,
+		y: state.y,
+		direction: newDirection
+	}
+}
+
+function move(state, speed) {
+	const [positive, leftOrRight] = (state.direction >>> 0).toString(2).padStart(2, "0").split("")
+	const isX = (leftOrRight === "1")
+
+	const d = (positive === "0" ? speed : -speed)
+	return {
+		x: state.x + (isX ? d : 0),
+		y: state.y + (!isX ? d : 0),
+		direction: state.direction
+	}
+}
 
 export default function RoverReducer(state, action) {
 
 	switch (action) {
-	case TURN_RIGHT: {
-		return {
-			x: state.x,
-			y: state.y,
-			direction: (state.direction === WEST) ? NORTH : state.direction + 1
-		}
+	case TURN_RIGHT:
+		return turn(state, 1)
 
-	}
+	case TURN_LEFT:
+		return turn(state, -1)
 
-	case TURN_LEFT: {
-		return {
-			x: state.x,
-			y: state.y,
-			direction: (state.direction === NORTH) ? WEST : state.direction - 1
-		}
+	case FORWARD:
+		return move(state, SPEED)
 
-	}
-
-	case FORWARD: {
-		let dx = 0
-		let dy = 0
-		switch (state.direction) {
-		case 0: {
-			dy = SPEED
-			break
-		}
-		case 1: {
-			dx = SPEED
-			break
-		}
-		case 2: {
-			dy = -SPEED
-			break
-		}
-		case 3: {
-			dx = -SPEED
-			break
-		}
-		}
-		return {
-			x: state.x + dx,
-			y: state.y + dy,
-			direction: state.direction
-		}
-	}
-
-	case BACKWARD: {
-		let dx = 0
-		let dy = 0
-		switch (state.direction) {
-		case 0: {
-			dy = -SPEED
-			break
-		}
-		case 1: {
-			dx = -SPEED
-			break
-		}
-		case 2: {
-			dy = SPEED
-			break
-		}
-		case 3: {
-			dx = SPEED
-			break
-		}
-		}
-		return {
-			x: state.x + dx,
-			y: state.y + dy,
-			direction: state.direction
-		}
-	}
+	case BACKWARD:
+		return move(state, -SPEED)
 
 	default:
 		return state
