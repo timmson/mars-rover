04d04f1cc7036240073b934d080204558fc905fb
timmson <timmson666@mail.ru>
2022-02-03 00:27:50 +0300

    Add TDD exercise

--- test/test-rover-reducer.js
+++ test/test-rover-reducer.js
@@ -3,36 +3,7 @@ import {EAST, NORTH, SOUTH, WEST} from "../src/directions"
 import {FORWARD, BACKWARD, TURN_RIGHT, TURN_LEFT} from "../src/actions"
 import {SPEED} from "../src/settings"*/
 
-/**
- * Your goal is to create RoverReducer in src/rover-reducer like that:
- * function RoverReducer(oldState, action) {
- *     ...
- *     return newState
- * }
- *
- * Examples:
- * state = {
- *     x: 0,
- *     y: 0,
- *     direction: EAST
- * }
- *
- * action is any of {FORWARD, BACKWARD, TURN_RIGHT, TURN_LEFT}
- *
- * Test cases:
- * [ ] When command is TURN_RIGHT Then rover should change direction clockwise
- * [ ] When command is TURN_LEFT Then rover should change direction counterclockwise
- * [ ] Think about refactoring
- *
- * [ ] When command is FORWARD Then rover should move to state.direction with SPEED
- * [ ] When command is BACKWARD Then rover should move from state.direction with SPEED
- * [ ] Think about refactoring
- *
- * To test your application in browser, use your real RoverReducer in app.js and run npm start
- */
-
 describe("RoverReduce should", () => {
-
 
 	test("do nothing", () => {
 		/*		const initialState = {state: "some state"}
