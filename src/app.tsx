215c6199d2b3df4776553c998070f684e07e35bb
timmson <timmson666@mail.ru>
2025-01-17 20:13:20 +0300

    - Prepare master

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
 import React, {useEffect, useReducer} from "react"
 
 import Rover from "./rover"
-import RoverReducer from "./rover-reducer"
+/*import RoverReducer from "./rover-reducer"*/
 import {EAST} from "./directions"
 import {MARS_HEIGHT, MARS_HEIGHT_SHIFT, ROVER_HEIGHT, ROVER_WIDTH} from "./settings"
+import {State} from "./state"
+
+const RoverReducer = (state:State) => {
+	return {...state}
+}
 
 type AppProps = {
 	window: Window,
